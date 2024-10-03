515820fb3fc388d1c57b5ab87cf6576c0da5d4be
Matthew Panizza <mlights999@gmail.com>
2024-10-03 18:25:38 -0400

    Import boards api and setup can controller

--- src/DecentralizedLV-HVController.cpp
+++ src/DecentralizedLV-HVController.cpp
@@ -13,16 +13,27 @@
 
 // Include Particle Device OS APIs
 #include "Particle.h"
+#include "DecentralizedLV-Boards/DecentralizedLV-Boards.h"
 
 // Let Device OS manage the connection to the Particle Cloud
 void setup();
 void loop();
-#line 13 "c:/Users/mligh/OneDrive/Particle/DecentralizedLV-HVController/src/DecentralizedLV-HVController.ino"
+#line 14 "c:/Users/mligh/OneDrive/Particle/DecentralizedLV-HVController/src/DecentralizedLV-HVController.ino"
 SYSTEM_MODE(MANUAL);
+
+CAN_Controller canController;
+
+DashController_CAN dashController(DASH_CONTROL_ADDR);
+
+PowerController_CAN powerController(POWER_CONTROL_ADDR);
 
 // setup() runs once, when the device is first turned on
 void setup() {
-  // Put initialization like pinMode and begin functions here
+    Serial.begin(115200);
+
+    canController.begin(CAN_500KBPS, S3);
+    canController.addFilter(powerController.boardAddress);   //Allow incoming messages from Power Controller
+    canController.addFilter(dashController.boardAddress);   //Allow incoming messages from Power Controller
 }
 
 // loop() runs over and over again, as quickly as it can execute.
